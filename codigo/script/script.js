b49f876459a7dd4ee7e416135331460898b1b2ba
Vitor <vitorcoelho5004@gmail.com>
2023-05-27 14:10:46 -0300

    filtro

--- codigo/script/script.js
+++ codigo/script/script.js
@@ -10,14 +10,49 @@ fetch('https://fakestoreapi.com/products')
                         <div class="card-body">
                             <p class="title">${values.title}</p>
                             <p class="card-text">${values.description}</p>
+                            <p class="card-category">Category:${values.category}</p>
                             <p class="card-price">${values.price}</p>
-                            <button class="price-button" type="submit">Adicionar ao carrinho</button>
+                            <p class="card-details"><a href="codigo/detalhes.html">See details</a></p>
+                            <button class="price-button" type="submit">Add to cart</button>
                         </div>
                 </div>`;
     });
 
     document.getElementById('cardContainer').innerHTML = data1;
+    if (data1 === "") {
+      alert("Nenhum produto encontrado para essa categoria.");
+    }    
+
+    // Filtrar produtos por categoria
+    const searchInput = document.querySelector('input[type="search"]');
+    const form = document.querySelector('form[role="search"]');
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const searchTerm = searchInput.value.toLowerCase();
+      const filteredData = completedata.filter((product) =>
+  product.category.toLowerCase().includes(searchTerm.toLowerCase())
+);
+
+
+      let filteredDataHTML = "";
+      filteredData.forEach((values) => {
+        filteredDataHTML += `<div class="card">
+                    <img src="${values.image}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="title">${values.title}</p>
+                            <p class="card-text">${values.description}</p>
+                            <p class="card-category">Category:${values.category}</p>
+                            <p class="card-price">${values.price}</p>
+                            <p class="card-details"><a href="codigo/detalhes.html">See details</a></p>
+                            <button class="price-button" type="submit">Add to cart</button>
+                        </div>
+                </div>`;
+      });
+
+      document.getElementById('cardContainer').innerHTML = filteredDataHTML;
+    });
   })
-.catch((err) => {
+  .catch((err) => {
     console.log(err);
-});
+    alert("Error to find your product, please reload the page")
+  });
