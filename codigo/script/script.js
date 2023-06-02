5f7823ccb260c0a2910057b248a6496d8cd4754f
Vitor <vitorcoelho5004@gmail.com>
2023-06-02 19:34:27 -0300

    final

--- codigo/script/script.js
+++ codigo/script/script.js
@@ -3,90 +3,41 @@ fetch('https://fakestoreapi.com/products')
     return data.json();
   })
   .then((completedata) => {
-    let data1 = "";
-    completedata.forEach((values) => {
-      data1 += `<div class="card">
-                    <img src="${values.image}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="title">${values.title}</p>
-                            
-                            
-                            <p class="card-price">$${values.price}</p>
-                            <p class="card-details"><a href="detalhes.html?id=${values.id}">See details</a></p>
-                            <button class="price-button" type="submit">Add to cart</button>
-                        </div>
-                </div>`;
-    });
+    let allData = completedata; // Armazena todos os dados iniciais
 
-    document.getElementById('cardContainer').innerHTML = data1;
-    if (data1 === "") {
-      alert("Nenhum produto encontrado para essa categoria.");
-    }    
-
-
-
-
-
-
-    // Filtrar produtos por categoria
-    const searchInput = document.querySelector('input[type="search"]');
-    const form = document.querySelector('form[role="search"]');
-    form.addEventListener('submit', (e) => {
-      e.preventDefault();
-      const searchTerm = searchInput.value.toLowerCase();
-      const filteredData = completedata.filter((product) =>
-  product.category.toLowerCase().includes(searchTerm.toLowerCase())
-);
-
-
-      let filteredDataHTML = "";
-      filteredData.forEach((values) => {
-        filteredDataHTML += `<div class="card">
-                    <img src="${values.image}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="title">${values.title}</p>
-                            <p class="card-text">${values.description}</p>
-                            <p class="card-category">Category:${values.category}</p>
-                            <p class="card-price">$${values.price}</p>
-                            <p class="card-details"><a href="detalhes.html?id=${values.id}">See details</a></p>
-                            <button class="price-button" type="submit">Add to cart</button>
-                        </div>
-                </div>`;
+    function renderData(data) {
+      let dataHTML = "";
+      data.forEach((values) => {
+        dataHTML += `<div class="card">
+          <img src="${values.image}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="title">${values.title}</p>
+            
+        
+            <p class="card-price">$${values.price}</p>
+            <p class="card-details"><a href="detalhes.html?id=${values.id}">See details</a></p>
+            <button class="price-button" type="submit">Add to cart</button>
+          </div>
+        </div>`;
       });
 
-      document.getElementById('cardContainer').innerHTML = filteredDataHTML;
+      document.getElementById('cardContainer').innerHTML = dataHTML;
+    }
+
+    renderData(allData); // Renderiza todos os produtos inicialmente
+
+    const searchInput = document.querySelector('input[type="search"]');
+    searchInput.addEventListener('input', () => {
+      const searchTerm = searchInput.value.toLowerCase();
+      const filteredData = allData.filter((product) =>
+        product.title.toLowerCase().includes(searchTerm) ||
+        product.category.toLowerCase().includes(searchTerm)
+      );
+
+      renderData(filteredData); // Renderiza os produtos filtrados por nome e categoria
     });
   })
   .catch((err) => {
     console.log(err);
-    alert("Error to find your product, please reload the page")
-  });
-
-
-  // Filtrar por nome
-  const searchInputByName = document.querySelector('#searchInput');
-  const formByName = document.querySelector('#seachButton');
-  formByName.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const searchTerm = searchInputByName.value.toLowerCase();
-    const filteredDataByName = completedata.filter((product) =>
-      product.title.toLowerCase().includes(searchTerm)
-    );
-
-    let filteredDataHTMLByName = "";
-    filteredDataByName.forEach((values) => {
-      filteredDataHTMLByName += `<div class="card">
-        <img src="${values.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="title">${values.title}</p>
-          <p class="card-text">${values.description}</p>
-          <p class="card-category">Category: ${values.category}</p>
-          <p class="card-price">$${values.price}</p>
-          <p class="card-details"><a href="detalhes.html?id=${values.id}">See details</a></p>
-          <button class="price-button" type="submit">Add to cart</button>
-        </div>
-      </div>`;
-    });
-
-    document.getElementById('cardContainer').innerHTML = filteredDataHTMLByName;
+    alert("Error to find your product, please reload the page");
   });
